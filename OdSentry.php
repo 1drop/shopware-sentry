813ef8ab8d20c452168daf6d6d32d8c63f303843
Hans Höchtl <hhoechtl@1drop.de>
2017-03-21 12:09:42 +0100

    [TASK] collect userfeedback configurable, JS inclusion improved, catch BE errors

--- OdSentry.php
+++ OdSentry.php
@@ -10,9 +10,6 @@ namespace OdSentry;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Enlight_Event_EventArgs;
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\RavenHandler;
-use Monolog\Logger;
 use OdSentry\Components\SentryClient;
 use Shopware\Components\Plugin;
 
@@ -21,7 +18,7 @@ class OdSentry extends Plugin
     /**
      * @var SentryClient
      */
-    protected $ravenClient;
+    protected $sentryClient;
 
     /**
      * @return array
@@ -30,15 +27,15 @@ class OdSentry extends Plugin
     {
         return [
             'Enlight_Controller_Front_StartDispatch' => 'onStartDispatch',
-//            'Enlight_Controller_Action_PreDispatch_Frontend_Error' => 'onPreDispatchError',
+            'Enlight_Controller_Action_PreDispatch_Frontend_Error' => 'onPreDispatchError',
             'Enlight_Controller_Action_PreDispatch_Backend_Error' => 'onPreDispatchBackendError',
             'Theme_Compiler_Collect_Plugin_Javascript' => 'addJsFiles',
+            'Theme_Compiler_Collect_Javascript_Files_FilterResult' => 'sortJs',
             'Enlight_Controller_Action_PostDispatch_Frontend' => 'onPostDispatchFrontend',
             'Enlight_Controller_Front_DispatchLoopShutdown' => 'onDispatchLoopShutdown',
             'Shopware_Console_Add_Command' => 'onStartDispatch'
         ];
     }
-
 
     /**
      * Use the autoloader from the Raven library to load all necessary classes
@@ -46,27 +43,17 @@ class OdSentry extends Plugin
     public function onStartDispatch()
     {
         require_once __DIR__ . '/vendor/autoload.php';
-        if (Shopware()->Config()->getByNamespace('OdSentry', 'logPhp')) {
-            $privateDsn = Shopware()->Config()->getByNamespace('OdSentry', 'privateDsn');
-            $this->ravenClient = new SentryClient($privateDsn, [
+        if (Shopware()->Config()->getByNamespace('OdSentry', 'sentryLogPhp')) {
+            $privateDsn = Shopware()->Config()->getByNamespace('OdSentry', 'sentryPrivateDsn');
+            $this->sentryClient = new SentryClient($privateDsn, [
                 'release' => \Shopware::VERSION,
                 'environment' => $this->container->getParameter('kernel.environment'),
                 'install_default_breadcrumb_handlers' => false
             ]);
-            $this->ravenClient->setContainer($this->container);
-
-            // Register additional handler for corelogger and pluginlogger
-            $ravenHandler = new RavenHandler($this->ravenClient, Logger::WARNING);
-            $ravenHandler->setFormatter(new LineFormatter("%message% %context% %extra%\n"));
-            /** @var Logger $coreLogger */
-            $coreLogger = $this->container->get('corelogger');
-            $coreLogger->pushHandler($ravenHandler);
-            /** @var Logger $pluginLogger */
-            $pluginLogger = $this->container->get('pluginlogger');
-            $pluginLogger->pushHandler($ravenHandler);
+            $this->sentryClient->setContainer($this->container);
 
             // Register global error handler
-            $errorHandler = new \Raven_ErrorHandler($this->ravenClient);
+            $errorHandler = new \Raven_ErrorHandler($this->sentryClient);
             $errorHandler->registerExceptionHandler();
             $errorHandler->registerShutdownFunction();
             // Restore Shopware default error handler
@@ -84,52 +71,76 @@ class OdSentry extends Plugin
     {
         /** @var \Shopware_Controllers_Frontend_Error $subject */
         $subject = $args->getSubject();
-        if ($this->ravenClient) {
+        if ($this->sentryClient) {
             $error = $subject->Request()->getParam('error_handler');
             if ($error && $error->exception) {
-                $this->ravenClient->captureException($error->exception);
+                $this->sentryClient->captureException($error->exception);
             }
         }
     }
 
-//    /**
-//     * @param Enlight_Event_EventArgs $args
-//     *
-//     * @return void
-//     */
-//    public function onPreDispatchError(Enlight_Event_EventArgs $args)
-//    {
-//        /** @var \Shopware_Controllers_Frontend_Error $subject */
-//        $subject = $args->getSubject();
-//        if ($this->ravenClient) {
-//            $error = $subject->Request()->getParam('error_handler');
-//            if ($error && $error->exception) {
-//                $id = $this->ravenClient->captureException($error->exception);
-//                $pluginConfig = $this->getConfig();
-//                if (!$pluginConfig['sentryUserfeedback']) {
-//                    return;
-//                }
-//                $this->container->get('template')->assignGlobal('sentryId', $id);
-//                $this->container->get('template')->assignGlobal('sentryUrlFeedback', $pluginConfig['sentryUrlFeedback']);
-//                $this->container->get('template')->addTemplateDir($this->getPath() . '/Resources/views');
-//            }
-//        }
-//    }
+    /**
+     * @param Enlight_Event_EventArgs $args
+     *
+     * @return void
+     */
+    public function onPreDispatchError(Enlight_Event_EventArgs $args)
+    {
+        /** @var \Shopware_Controllers_Frontend_Error $subject */
+        $subject = $args->getSubject();
+        if ($this->sentryClient) {
+            $error = $subject->Request()->getParam('error_handler');
+            if ($error && $error->exception) {
+                $id = $this->sentryClient->captureException($error->exception);
+                if (!Shopware()->Config()->getByNamespace('OdSentry', 'sentryUserfeedback')) {
+                    return;
+                }
+                $this->container->get('template')->assignGlobal('sentryId', $id);
+                $this->container->get('template')->addTemplateDir($this->getPath() . '/Resources/views/');
+            }
+        }
+    }
 
     /**
-     *
+     * Add the raven-js library to the compiled JS.
+     * It will we reordered by the sortJs function to be included
+     * before the bootstrapping of most JS to track these errors too.
      *
      * @return ArrayCollection
      */
     public function addJsFiles()
     {
-        // todo: include raven js before jquery plugins to detect errors in plugin bootstrapping
+        $jsFiles = [];
         $jsDir = __DIR__ . '/Resources/views/frontend/_public/src/js/';
-        $jsFiles = [
-            $jsDir . 'vendor/raven.min.js',
-        ];
-
+        if (Shopware()->Config()->getByNamespace('OdSentry', 'sentryLogJs') || Shopware()->Config()->getByNamespace('OdSentry', 'sentryUserfeedback')) {
+            $jsFiles[] = $jsDir . 'vendor/raven.min.js';
+        }
         return new ArrayCollection($jsFiles);
+    }
+
+    /**
+     * Sort the raven-js library to the front of the JS compilation pipeline
+     * so that it can track errors in the initialization of other JS libraries.
+     *
+     * @param Enlight_Event_EventArgs $args
+     * @return array
+     */
+    public function sortJs(\Enlight_Event_EventArgs $args)
+    {
+        $files = $args->getReturn();
+        $fileIdx = -1;
+        foreach ($files as $idx => $file) {
+            if (strpos($file, 'raven.min.js') !== false && strpos($file, 'OdSentry') !== false) {
+                $fileIdx = $idx;
+                break;
+            }
+        }
+        if ($fileIdx > -1) {
+            $tmp = array_splice($files, $fileIdx, 1);
+            // the 5th position is usually after the vendor libraries
+            array_splice($files, 5, 0, $tmp);
+        }
+        return $files;
     }
 
     /**
@@ -142,11 +153,11 @@ class OdSentry extends Plugin
     {
         $response = $args->getSubject()->Response();
         $exceptions = $response->getException();
-        if (empty($exceptions) || !$this->ravenClient instanceof \Raven_Client) {
+        if (empty($exceptions) || !$this->sentryClient) {
             return;
         }
         foreach ($exceptions as $exception) {
-            $this->ravenClient->captureException($exception);
+            $this->sentryClient->captureException($exception);
         }
     }
 
--- Resources/views/frontend/index/index.tpl
+++ Resources/views/frontend/index/index.tpl
@@ -2,11 +2,23 @@
 
 {block name="frontend_index_header_javascript_jquery_lib"}
     {$smarty.block.parent}
-    {if {config name='logJs'}}
-    <script type="text/javascript">
-        if (Raven) {
-            Raven.config('{config name='publicDsn'}').install();
-        }
-    </script>
+    {* If sentry tracking of JS errors is enabled *}
+    {if {config name='sentryLogJs'}}
+        <script type="text/javascript">
+            if (Raven) {
+                Raven.config('{config name='sentryPublicDsn'}').install();
+            }
+        </script>
+    {/if}
+    {* If a PHP error occured and we want to collect additional feedback *}
+    {if $sentryId}
+        <script>
+            if (Raven) {
+                Raven.showReportDialog({
+                    eventId: '{$sentryId}',
+                    dsn: '{config name='sentryPublicDsn'}'
+                });
+            }
+        </script>
     {/if}
 {/block}
